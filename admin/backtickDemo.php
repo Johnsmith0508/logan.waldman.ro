fc657d1df9c5da27b40b1b6fdaf030e6eedba45d
Johnsmith0508 <loganwaldman@gmail.com>
2015-02-19 08:44:52 -0500

    fixed stuff

--- admin/backtickDemo.php
+++ admin/backtickDemo.php
@@ -2,8 +2,8 @@
 	<body>
 		<?php
 echo `/usr/local/Cellar/git/2.3.0/bin/git pull 2>&1`;
-$myfile = fopen("newfile.txt", "r+") or die("Unable to open file!");
-fwrite($myfile, $_POST);
+$myfile = fopen("demo.json", "w") or die("Unable to open file!");
+fwrite($myfile, file_get_contents('php://input'));
 fclose($myfile);
 		?>
 	</body>
