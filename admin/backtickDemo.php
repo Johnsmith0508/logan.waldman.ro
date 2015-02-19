18f4488bcfb0bc825af59c4df26eeae23e3c0710
Johnsmith0508 <loganwaldman@gmail.com>
2015-02-19 07:30:36 -0500

    forgot a sem

--- admin/backtickDemo.php
+++ admin/backtickDemo.php
@@ -2,8 +2,8 @@
 	<body>
 		<?php
 echo `/usr/local/Cellar/git/2.3.0/bin/git pull 2>&1`;
-$stdout = fopen("push.json","w");
-fwrite($stdout,$_POST)
+$stdout = fopen("push.json","w") or die("cant open that");
+fwrite($stdout,$_POST);
 fclose($stdout);//just to update
 		?>
 	</body>
