fed541238d22701c42424d35c55715f5d9d48e22
Johnsmith0508 <loganwaldman@gmail.com>
2015-02-19 08:29:42 -0500

    noop

--- admin/backtickDemo.php
+++ admin/backtickDemo.php
@@ -2,7 +2,7 @@
 	<body>
 		<?php
 echo `/usr/local/Cellar/git/2.3.0/bin/git pull 2>&1`;
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$myfile = fopen("newfile.txt", "r+") or die("Unable to open file!");
 fwrite($myfile, $_POST);
 fclose($myfile);
 		?>
