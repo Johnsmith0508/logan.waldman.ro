ff4bbab8681bd6d5e6c4ceccc84e4dec386d0396
Johnsmith0508 <loganwaldman@gmail.com>
2015-02-19 07:31:53 -0500

    can we get an  update?

--- admin/backtickDemo.php
+++ admin/backtickDemo.php
@@ -4,7 +4,7 @@
 echo `/usr/local/Cellar/git/2.3.0/bin/git pull 2>&1`;
 $stdout = fopen("push.json","w") or die("cant open that");
 fwrite($stdout,$_POST);
-fclose($stdout);//just to update
+fclose($stdout);
 		?>
 	</body>
 </html>
