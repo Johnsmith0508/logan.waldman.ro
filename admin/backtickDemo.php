707991bcd99f27471312cc70a442fec084718fb2
Johnsmith0508 <loganwaldman@gmail.com>
2015-02-19 08:26:31 -0500

    meh

--- admin/backtickDemo.php
+++ admin/backtickDemo.php
@@ -2,9 +2,9 @@
 	<body>
 		<?php
 echo `/usr/local/Cellar/git/2.3.0/bin/git pull 2>&1`;
-$stdout = fopen("push.json","w") or die("cant open that");
-fwrite($stdout,$_POST);
-fclose($stdout);
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $_POST);
+fclose($myfile);
 		?>
 	</body>
 </html>
